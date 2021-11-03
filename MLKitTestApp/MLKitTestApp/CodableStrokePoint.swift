dec32c15bea6fbf6afec6335f7f54fe79fbcdda2
Sergii Sopin <Sergii_Sopin@epam.com>
2021-11-02 21:22:47 -0400

    Failing project example added

---
+++ MLKitTestApp/MLKitTestApp/CodableStrokePoint.swift
@@ -0,0 +1,8 @@
+//
+//  CodableStrokePoint.swift
+//  MLKitTestApp
+//
+//  Created by Sergii Sopin on 2021-11-02.
+//
+
+import Foundation
---
+++ MLKitTestApp/MLKitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MLKitTestApp
+//
+//  Created by Sergii Sopin on 2021-11-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
