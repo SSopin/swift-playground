0918bba9a065e38f5a8eb368d3ac1d6af39a00b1
Sergii Sopin <Sergii_Sopin@epam.com>
2021-11-11 08:16:49 -0500

    Very inefficient, but working fix

--- MLKitTestApp/MLKitTestApp/Line.swift
+++ MLKitTestApp/MLKitTestApp/Line.swift
@@ -9,7 +9,7 @@ import Foundation
 import MLKit
 
 struct Line: Codable {
-    var strokes: [SerializableStroke] = []
+    var strokes: [Stroke] = []
     var text: String = ""
     
     public init() {
@@ -17,15 +17,55 @@ struct Line: Codable {
     
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(NSKeyedArchiver.archivedData(withRootObject: SwiftInk(base: SerializableInk(strokes: strokes)), requiringSecureCoding: false), forKey: .strokes)
+        try container.encode(Ink.init(strokes: strokes).toCodable, forKey: .ink)
     }
     
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
-        self.strokes = try values.decode(SwiftInk.self, forKey: .strokes).wrapped.strokes as? [SerializableStroke] ?? []
+        self.strokes = try Ink.init(fromCodable: values.decode(Ink.CodableInk.self, forKey: .ink)).strokes
     }
 
     enum CodingKeys: String, CodingKey {
-        case strokes
+        case ink
+    }
+}
+
+//MARK: - Extensions to make Inc codable
+
+extension StrokePoint {
+    struct CodableStrokePoint: Codable {
+        let x: Float
+        let y: Float
+        let t: Int
+    }
+    var toCodable: CodableStrokePoint {
+        .init(x: x, y: y, t: t?.intValue ?? 0)
+    }
+    convenience init(fromCodable pt: CodableStrokePoint) {
+        self.init(x: pt.x, y: pt.y, t: pt.t)
+    }
+}
+
+extension Stroke {
+    struct CodableStroke: Codable {
+        let points: [StrokePoint.CodableStrokePoint]
+    }
+    var toCodable: CodableStroke {
+        .init(points: points.map {$0.toCodable} )
+    }
+    convenience init(fromCodable pt: CodableStroke) {
+        self.init(points: pt.points.map { StrokePoint.init(fromCodable: $0)} )
+    }
+}
+
+extension Ink {
+    struct CodableInk: Codable {
+        let strokes: [Stroke.CodableStroke]
+    }
+    var toCodable: CodableInk {
+        .init(strokes: strokes.map {$0.toCodable} )
+    }
+    convenience init(fromCodable pt: CodableInk) {
+        self.init(strokes: pt.strokes.map { Stroke.init(fromCodable: $0)} )
     }
 }
--- MLKitTestApp/MLKitTestApp/MLKitTestApp-Bridging-Header.h
+++ MLKitTestApp/MLKitTestApp/MLKitTestApp-Bridging-Header.h
@@ -1,7 +1,7 @@
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
-
-#import "SerializableStrokePoint.h"
-#import "SerializableStroke.h"
-#import "SerializableInk.h"
+//
+//#import "SerializableStrokePoint.h"
+//#import "SerializableStroke.h"
+//#import "SerializableInk.h"
